3bc3110dc8a82bfff22784d1ba1c3fa2674767cb
Alexander Foster <7709@balcarras.gloucs.sch.uk>
2025-03-10 22:55:48 +0000

    unimportant tweaks

--- main.py
+++ main.py
@@ -2,6 +2,79 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
+def findEmpty(bo):
+    for r in range(9):
+        for c in range(9):
+            if bo[r][c]==0:
+                return r,c
+    return False
+#returns row and column of first empty cell
+
+
+def checkValid(bo, num, pos):
+    #check row
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i]==num and pos[1]!=i:
+            return False
+
+    #check column
+    for i in range(len(bo)):
+        if bo[i][pos[1]]==num and pos[0]!=i:
+            return False
+
+    #check 3x3 grid
+    box_x=pos[1]//3
+    box_y=pos[0]//3
+
+    for i in range(box_y*3, box_y*3+3):
+        for j in range(box_x*3, box_x*3+3):
+            if bo[i][j]==num and (i, j)!=pos:
+                return False
+
+    return True
+
+
+def solve(bo):
+    find=findEmpty(bo)
+    if not find:
+        return True  #no empty cells left, puzzle solved
+    else:
+        row, col=find
+
+    for i in range(1, 10):
+        if checkValid(bo, i, (row, col)):
+            bo[row][col]=i
+
+            if solve(bo):
+                #print("boom")
+                return bo
+
+            bo[row][col]=0  #reset the cell and backtrack
+
+    return False  #trigger backtracking
+
+
+
+
+def display(bo):
+    for i in range(9):
+        if i%3==0 and i!=0:
+            print("- - - - - - - - - - - - - ")
+        for j in range(9):
+            if j%3==0 and j!=0:
+                print(" | ", end="")
+            if j==8:
+                print(bo[i][j])
+            else:
+                print(str(bo[i][j])+" ", end="")
+#displays the board; uses integer division to determine when to print horizontal and vertical lines
+
+
+
+
+
+
 @app.route("/", methods = ["get","post"])
 def displayGrid():
 
@@ -9,6 +82,7 @@ def displayGrid():
         #work out how to get all form data and turn it into a list
         #compare this new list with the solution
         #create a new list with indication of correct or not
+        #iterate through this new list to decide what colour to highlight cells
 
         gridData = [
             [[1,False],"","","","","","","",""],
@@ -25,18 +99,7 @@ def displayGrid():
     else:
         ##Generate a new sudoku
         ##Form this as a grid and send the solution and initial grid
-
-        solution = [
-            ["1","","","","","","","",""],
-            ["","","","3","","","","",""],
-            ["","","","","","","","",""],
-            ["","","","","","","","",""],
-            ["","","","","","7","","",""],
-            ["","","","","","","","",""],
-            ["","","","4","","","","",""],
-            ["","","","","","","","",""],
-            ["","","","","","","","",""],
-        ]
+        
         gridData=[
             [8,0,0,4,0,6,0,0,7],
             [0,0,0,0,0,0,4,0,0],
@@ -48,6 +111,11 @@ def displayGrid():
             [0,0,1,0,0,0,0,0,0],
             [3,0,0,9,0,2,0,0,5]
         ]
+
+        empty=[row[:] for row in gridData]
+        solution=solve(empty)
+        
+
         
         
     return render_template("solution.html", gridData = gridData, solution = solution)
@@ -56,75 +124,13 @@ def displayGrid():
 
 
 
-
-app.run(debug = True)
-
+app.run(debug = True)  
 
 
-# def display(bo):
-#     for i in range(9):
-#         if i%3==0 and i!=0:
-#             print("- - - - - - - - - - - - - ")
-#         for j in range(9):
-#             if j%3==0 and j!=0:
-#                 print(" | ", end="")
-#             if j==8:
-#                 print(bo[i][j])
-#             else:
-#                 print(str(bo[i][j])+" ", end="")
-# #displays the board; uses integer division to determine when to print horizontal and vertical lines
 
 
-# def findEmpty(bo):
-#     for r in range(9):
-#         for c in range(9):
-#             if bo[r][c]==0:
-#                 return r,c
-#     return False
-# #returns row and column of first empty cell
 
 
-# def checkValid(bo, num, pos):
-#     #check row
-#     for i in range(len(bo[0])):
-#         if bo[pos[0]][i]==num and pos[1]!=i:
-#             return False
-
-#     #check column
-#     for i in range(len(bo)):
-#         if bo[i][pos[1]]==num and pos[0]!=i:
-#             return False
-
-#     #check 3x3 grid
-#     box_x=pos[1]//3
-#     box_y=pos[0]//3
-
-#     for i in range(box_y*3, box_y*3+3):
-#         for j in range(box_x*3, box_x*3+3):
-#             if bo[i][j]==num and (i, j)!=pos:
-#                 return False
-
-#     return True
-
-
-# def solve(bo):
-#     find=findEmpty(bo)
-#     if not find:
-#         return True  #no empty cells left, puzzle solved
-#     else:
-#         row, col=find
-
-#     for i in range(1, 10):
-#         if checkValid(bo, i, (row, col)):
-#             bo[row][col]=i
-
-#             if solve(bo):
-#                 print("boom")
-#                 return bo
-
-#             bo[row][col]=0  #reset the cell and backtrack
-
-#     return False  #trigger backtracking
 
 
 
